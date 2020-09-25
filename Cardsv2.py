e9c18b1cbbde8543f10b11be93fefc342a43c973
Kev Toh <69605400+tohkev@users.noreply.github.com>
2020-09-24 20:37:35 -0600

    Update and rename Cards.py to Cardsv2.py
    Now Cardsv2, which quantifies the suits (Spades, Clubs, Hearts, Diamonds) for the purpose of the game.

--- Cardsv2.py
+++ Cardsv2.py
@@ -1,8 +1,6 @@
 import random
 from operator import attrgetter                                     #used to sort cards
 
-suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
-
 class Card:
     def __init__(self, value, suit):
         self.value = value
@@ -10,14 +8,24 @@ class Card:
 
     def __repr__(self):
         if self.value == 14:
-            self.value = "Ace"
+            value = "Ace"
         elif self.value == 13:
-            self.value = "King"
+            value = "King"
         elif self.value == 12:
-            self.value = "Queen"
+            value = "Queen"
         elif self.value == 11:
-            self.value = "Jack"
-        return "{} of {}". format(self.value, self.suit)
+            value = "Jack"
+        else:
+            value = self.value
+        if self.suit == 0:
+            suit = "Spades"
+        if self.suit == 1:
+            suit = "Clubs"
+        if self.suit == 2:
+            suit = "Diamonds"
+        if self.suit == 3:
+            suit = "Hearts"
+        return "{} of {}". format(self.value, suit)
 
     def __eq__(self, other):                                        #used to compare cards
         if self.suit == other.suit and self.value == other.value:
@@ -32,7 +40,7 @@ class Deck:
         self.build()
 
     def build(self):
-        for suit in suits:
+        for suit in range(0, 4):
             for value in range(2, 15):
                 self.cards.append(Card(value, suit))
 
@@ -77,7 +85,32 @@ class Player:
                 card.value = "King"
             elif card.value == 14:
                 card.value = "Ace"
+            if card.suit == 0:
+                card.suit = "Spades"
+            if card.suit == 1:
+                card.suit = "Clubs"
+            if card.suit == 2:
+                card.suit = "Diamonds"
+            if card.suit == 3:
+                card.suit = "Hearts"
             card.show()
+        for card in self.hand:
+            if card.value == "Jack":
+                card.value = 11
+            elif card.value == "Queen":
+                card.value = 12
+            elif card.value == "King":
+                card.value = 13
+            elif card.value == "Ace":
+                card.value = 14
+            if card.suit == "Spades":
+                card.suit = 0
+            if card.suit == "Clubs":
+                card.suit = 1
+            if card.suit == "Diamonds":
+                card.suit = 2
+            if card.suit == "Hearts":
+                card.suit = 3
 
     def checkwins(self):
         print("You have " + str(self.win) + " wins!")
@@ -88,7 +121,7 @@ class Player:
 
     def discard(self, value, suit):
         for card in self.hand:
-            if card.value == value and card.suit == suit.title():
+            if card.value == value and card.suit == suit:
                 self.hand.remove(card)
 
     def emptyhand(self):
